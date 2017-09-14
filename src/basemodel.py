09c6cd5d3dfc8347da7c6da8887a10317d09c1e4
lumenghe <lumenghe@gmail.com>
2017-09-14 09:39:33 +0100

    adding file, required libs, define new class 'BaseModel'

---
+++ src/basemodel.py
@@ -0,0 +1,20 @@
+import os
+import gc
+from eval import full_eval, submit_for_eval
+from data import *
+from preprocessing import preprocess
+
+
+class BaseModel:
+    def __init__(self, config):
+        self.config = config
+        self.xtrain = None
+        self.ytrain = None
+        self.xvalid = None
+        self.yvalid = None
+        self.xtest = None
+        self.ytest = None
+        self.xsubmit = None
+        self.submitindex = None
+        self.params = config["params"]
+        return
