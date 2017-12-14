a17877c830b241da25f25379cdc75d12a71bf838
lumenghe <lumenghe@gmail.com>
2017-12-14 10:35:51 +0000

    defining environment

--- src/model_xgb_sign.py
+++ src/model_xgb_sign.py
@@ -17,3 +17,18 @@ class Model(BaseModel):
         self.settings["silent"] = 1
         self.num_round = config["num_round"]
         self.early_stopping_rounds = config["early_stopping_rounds"]
+
+    def init_train_data(self):
+        super(Model, self).init_train_data_base()
+
+    def train(self):
+        print("Initializing train data...")
+        self.init_train_data()
+        print("Start model fitting...")
+        t = time.time()
+        xgb_train = xgb.DMatrix(self.xtrain.values, label=self.ytrain.values)
+        xgb_valid = xgb.DMatrix(self.xvalid.values, label=self.yvalid.values)
+        watchlist = [(xgb_train, "train"), (xgb_valid, "valid")]
+        self.model = xgb.train(self.settings, xgb_train, self.num_round, watchlist, early_stopping_rounds=self.early_stopping_rounds, verbose_eval=10)
+        total_time = int(time.time() - t)
+        print("Trained model in {} secs".format(total_time))
