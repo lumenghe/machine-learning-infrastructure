05fd80fa82a406867a416f219288ebf50d4cafe6
lumenghe <lumenghe@gmail.com>
2018-01-11 10:21:48 +0000

    new function 'save'

--- src/model_xgb_sign.py
+++ src/model_xgb_sign.py
@@ -36,12 +36,3 @@ class Model(BaseModel):
     def save(self):
         self.model.save_model(self.params)
         print("Saved model at: {}".format(self.params))
-
-    def load(self):
-        self.model = xgb.Booster(model_file=self.params)
-        print("Loaded model from: {}".format(self.params))
-
-    def predict_from_x(self, x):
-        xbg_data = xgb.DMatrix(x)
-        pred = self.model.predict(xbg_data)
-        return pred
