6b295d14b2c6945fcc9af5badd90864de0d21ab8
lumenghe <lumenghe@gmail.com>
2018-02-01 09:55:31 +0000

    new function 'get_period_info'

--- src/stats.py
+++ src/stats.py
@@ -24,3 +24,25 @@ def print_info(plot=False):
       #  sub_df = df.loc[df.apply(lambda row: row['transactiondate'].month, axis=1) == i]
         get_period_info(df, begin, end, color[i%8], plot)
 
+
+def get_period_info(df, start_date, end_date, color, plot=False):
+    sub_df = df.loc[df.apply(lambda row: start_date <= row['transactiondate'] < end_date, axis=1)]
+    mu = sub_df['logerror'].mean()
+    sig = sub_df['logerror'].std()
+    histogram_df = pd.DataFrame({'logerror': sub_df['logerror']})
+
+    if plot:
+        plt.hist(histogram_df.values, bins='auto', range=(-0.5, 0.5), normed=True)
+        x = np.linspace(-0.5, 0.5, num=100)
+        plt.plot(x, gaussian(x, mu, sig), color)
+        plt.show()
+        sm.qqplot(histogram_df.values, line='45', fit=True)
+        plt.show()
+
+    vals = histogram_df.values
+    quartile_1 = histogram_df.quantile(q=0.25)['logerror']
+    median = histogram_df.quantile(q=0.5)['logerror']
+    quartile_3 = histogram_df.quantile(q=0.75)['logerror']
+    print("[{0} {1}) mean = {2}, std = {3}, quartile_1 = {4}, median = {5}, quartile_1 = {6}, SKEW = {7} KURTOSIS = {8} data len={9}".format(
+    start_date, end_date, mu, sig, quartile_1 , median, quartile_3 , skew(vals), kurtosis(vals), len(sub_df.index)))
+
